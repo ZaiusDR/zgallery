6efe85463472833b7d2a55614ecccdd7305bf7ee
Eduardo Suarez <eduardosuarez@gmail.com>
2019-12-18 17:55:20 +0000

    Testing integration between services

--- zgallery-frontend/src/App.js
+++ zgallery-frontend/src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>holi</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: [],
+    }
+  };
+
+  componentDidMount() {
+    fetch('http://zgallery-backend:3000/api/v1/albums')
+      .then(results => {
+        return results.json()
+      }).then(data => {
+        let albums = data.results.map(album => {
+          return(
+            <div key={album}>
+              <p>album</p>
+            </div>
+          )
+        });
+        this.setState({ albums: albums})
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="Albums-container">
+            {this.state.albums}
+          </div>
+          <p>holi</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
